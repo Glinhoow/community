bb5f12cac68c765de70139fe333e27b05cc8883c
Glinhoow <1105109761@qq.com>
2019-09-06 23:11:42 +0800

    完成登录功能

--- src/main/java/com/example/community/controller/AuthorizeController.java
+++ src/main/java/com/example/community/controller/AuthorizeController.java
@@ -1,16 +1,55 @@
 package com.example.community.controller;
 
+import com.example.community.dto.AccessTokenDTO;
+import com.example.community.dto.GithubUser;
+import com.example.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name ="state") String state
-                           ){
-        return "index";
+                           @RequestParam(name ="state") String state,
+                           HttpServletRequest request){  //因为session是在HttpServletRequest里面拿到的
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setState(state);
+
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        //拿到用户信息
+        GithubUser user = githubProvider.getUser(accessToken);
+
+        if (user != null) {
+            //登录成功，写cookies跟session
+            //把user放到session里面
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }else {
+            //登录失败，重新登录
+            return "redirect:/";
+
+        }
     }
 
 }
